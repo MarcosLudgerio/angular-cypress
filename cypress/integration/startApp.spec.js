3d1f7cac246c1d5a95d2b7c6d5224129f96d5190
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-19 15:43:24 -0300

    feat: added fixture for clean the test

--- cypress/integration/startApp.spec.js
+++ cypress/integration/startApp.spec.js
@@ -2,9 +2,15 @@ describe("App intialization", () => {
 
     it("Displays todos from API on load", () => {
         cy.server(); // Start a server to begin routing responses to cy.route() and to change the behavior of network requests.
-        cy.route('GET', '/ToDoModels', []); // To manage the behavior of network requests.
+        /**
+         * fixture('todos').then(res => {
+         * cy.route('GET', '/ToDoModels', res); // To manage the behavior of network requests.
+        });
+        */
+        cy.route('GET', '/ToDoModels', "fixture:todos"); // Alternative form for execute the test
+
         cy.visit("/");
-        cy.get('.task-wrapper').should('have.length', 3);
+        cy.get('.task-wrapper').should('have.length', 5);
     });
 
 });
