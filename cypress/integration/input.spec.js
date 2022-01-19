d601554357efe49062fcc7752eaaa3544991c5c5
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-19 15:21:09 -0300

    feat: implemented test type in input

--- cypress/integration/input.spec.js
+++ cypress/integration/input.spec.js
@@ -1,6 +1,10 @@
 describe("Form input", () => {
     it("Focuses the input on load", () => {
-        cy.visit("/");
         cy.focused().should("have.id", "title"); // Focused: Get the DOM element that is currently focused.
+    });
+
+    it.only("Accepts input", () => {
+        cy.visit("/");
+        cy.get('#title').type("New todo").should('have.value', "New todo");
     });
 });
