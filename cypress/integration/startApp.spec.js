60231a1ea9ad439cf96f2d80b6389b3f8c6c810f
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-19 15:50:55 -0300

    feat: implemented as and wait

--- cypress/integration/startApp.spec.js
+++ cypress/integration/startApp.spec.js
@@ -7,8 +7,9 @@ describe("App intialization", () => {
          * cy.route('GET', '/ToDoModels', res); // To manage the behavior of network requests.
         });
         */
-        cy.route('GET', '/ToDoModels', "fixture:todos"); // Alternative form for execute the test
-
+        cy.route('GET', '/ToDoModels', "fixture:todos").as("load"); // Alternative form for execute the test 
+        // as: Assign an alias for later use. Reference the alias later within a cy.get() or cy.wait() command with an @ prefix.
+        cy.wait("@load"); // wait: Wait for a number of milliseconds or wait for an aliased resource to resolve before moving on to the next command.
         cy.visit("/");
         cy.get('.task-wrapper').should('have.length', 5);
     });
