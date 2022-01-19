9c8b9255e56d6ba80a5dce1b220ec35476c96524
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-19 15:37:12 -0300

    feat: implemented mock server

--- cypress/integration/startApp.spec.js
+++ cypress/integration/startApp.spec.js
@@ -1,10 +1,10 @@
 describe("App intialization", () => {
 
-    beforeEach(() => {
-        cy.visit("/");
-    });
-
     it("Displays todos from API on load", () => {
+        cy.server(); // Start a server to begin routing responses to cy.route() and to change the behavior of network requests.
+        cy.route('GET', '/ToDoModels', []); // To manage the behavior of network requests.
+        cy.visit("/");
         cy.get('.task-wrapper').should('have.length', 3);
     });
+
 });
