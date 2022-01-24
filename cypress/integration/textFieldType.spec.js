1e9258f28b5da376c9dc5ba06ce571f6794a046d
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-24 11:54:22 -0300

    feat: starts a test update todo

--- cypress/integration/textFieldType.spec.js
+++ cypress/integration/textFieldType.spec.js
@@ -29,7 +29,11 @@ describe("Typing in input", () => {
 
 
     it.only("Edit a todo item", () => {
-        cy.get('.task-wrapper button.edit').first().click();
+        cy.get('.task-wrapper button.edit').first().click(); // Click in edit button
+        cy.get("#title").clear().type(newTodo.title).type('{enter}'); // update title task
+        // cy.wait('@save');
+        // cy.wait('@second-load');
+        // cy.get().to.be(); // assert if update is sucessful
     });
 
 });
